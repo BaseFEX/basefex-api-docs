31e1c22ea9158b254f67188b8e0063e5630880b7
Isaac Zeng <ndtm.idea@gmail.com>
2018-12-07 16:48:17 +0800

    Delete .dir-locals.el

--- .dir-locals.el
+++
@@ -1,4 +0,0 @@
-((markdown-mode . ((markdown-toc-user-toc-structure-manipulation-fn . (lambda (toc-structure)
-                                                                        (->> toc-structure
-                                                                             (remove-if (lambda (e) (zerop (car e))))
-                                                                             (mapcar (lambda (e) (cons (1- (car e)) (cdr e))))))))))
